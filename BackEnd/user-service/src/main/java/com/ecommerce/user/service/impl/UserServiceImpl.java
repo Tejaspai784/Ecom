eb19b25243f682a9c9089be329946b957c8ddaa8
Tejaspai784 <tejaspai2003@gmail.com>
2026-04-01 16:41:41 +0530

    Order & Product services added

--- BackEnd/user-service/src/main/java/com/ecommerce/user/service/impl/UserServiceImpl.java
+++ BackEnd/user-service/src/main/java/com/ecommerce/user/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.ecommerce.user.repository.UserRepository;
 import com.ecommerce.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,12 +14,13 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     @Override
+    @SuppressWarnings("null")
     public User registerUser(User user) {
         return userRepository.save(user);
     }
 
     @Override
     public User getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+        return Optional.ofNullable(userRepository.findByEmail(email)).orElse(null);
     }
 }
